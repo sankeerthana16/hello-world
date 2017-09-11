36848666ab92b5670a4b1ca00d43c1812465c0a5
sankeerthana16 <31802128+sankeerthana16@users.noreply.github.com>
2017-09-11 13:53:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		public void setStudents(Student[] students) {
+		// Add your implementation here
+		Scanner sc=new Scanner("System.in");
+		int id=sc.nextInt();
+		String fullname=sc.nextLine();
+		Date birthDate=sc.nextLine();
+		double avgMark=sc.nextDouble();
+	}
+	Main m=new Main();
+	m.setStudents();
+	}
+
+}
